6a5926947187246aaed9c6edb2600fd3deb59cb7
onur yucel <onuryucel200@hotmail.com>
2025-05-29 21:55:14 +0300

    v0.4 duzenleme

---
+++ Assembler/Fileio/FileReader.h
@@ -0,0 +1,25 @@
+#pragma once
+
+//res
+/*
+https://www.youtube.com/watch?v=JaT0Fnux1nQ&list=PLRwVmtr-pp06qT6ckboaOhnm9FxmzHpbY&index=20
+*/
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+class FileReader
+{
+public:
+	FileReader();
+	~FileReader();
+	
+	std::string read(const char* path);	
+
+	void write(const char* path, std::string& data, std::ios::openmode mode);
+
+	
+private:
+	
+};
